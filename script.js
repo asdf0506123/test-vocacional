d51f55b7ab0f208ce84ea23b866d1e8a9a6fa717
Melanie Alcocer <168877548+asdf0506123@users.noreply.github.com>
2025-09-04 12:41:54 -0600

    padre nuestro

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ function lanzarConfeti() {
 }
 
 function showResults(puntajes) {
-    resultsDiv.innerHTML = '<h2 style="color: #fff;">Las carreras que más coinciden con tus intereses y habilidades son:</h2>';
+    resultsDiv.innerHTML = '<h2 style="color: #fff;">Las carreras que más coinciden con tus intereses son:</h2>';
     testForm.classList.add('hidden');
     progressBar.classList.add("hidden");
 
